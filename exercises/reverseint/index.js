660c1bb7da29825799001b84e17ca190f5818a39
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-08-06 15:58:18 -0400

    implement reverse number

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversedInt = 0;
+  let isNegative = false;
+
+  if (n < 0) {
+    n = Math.abs(n);
+    isNegative = true;
+  }
+
+  while (n > 0) {
+    reversedInt = reversedInt * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+
+  if (isNegative) {
+    return -reversedInt;
+  }
+  return reversedInt;
+}
 
 module.exports = reverseInt;

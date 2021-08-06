3c6fe8ecb16840e4f4b92db853424f24242bf6f9
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-08-06 15:04:51 -0400

    implement reverse using reduce

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,21 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reverse().join('');
+  // return str.split('').reverse().join('');
+  return str.split('').reduce((reveserd, char) => char + reveserd, '');
+
+  // const strArray = str.split('');
+  // let reveserd = '';
+
+  // for (let i = strArray.length - 1; i >= 0; i--) {
+  //   reveserd += strArray[i];
+  // }
+
+  // for (let char of str) {
+  //   reveserd = char + reveserd;
+  // }
+
+  // return reveserd;
 }
 
 module.exports = reverse;

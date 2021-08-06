c74f4d697876fdc07585576d62e3b1d77c17f88d
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-08-06 15:29:10 -0400

    implement palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // let palindrome = str.split('').reduce((rev, char) => char + rev, '');
+  // return palindrome === str;
+
+  return str.split('').every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
+}
 
 module.exports = palindrome;

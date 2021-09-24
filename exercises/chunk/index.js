189bd7c3aac604491956a839e5c8c78a78a25089
Raju <gharti.rg@gmail.com>
2021-09-24 08:54:11 -0400

    implement array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const results = [];
+  while (array.length) {
+    results.push(array.slice(0, size));
+    array = array.slice(size);
+  }
+
+  return results;
+}
 
 module.exports = chunk;

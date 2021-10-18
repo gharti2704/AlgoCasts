e09876446815b770d2f93ff86049fdb60469faec
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-10-18 11:39:24 -0400

    implement weave method

---
+++ exercises/queue/weave.js
@@ -0,0 +1,19 @@
+import Queue from './index';
+
+const weave = (sourceOne, sourceTwo) => {
+  const q = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
+  }
+
+  return q;
+};
+
+module.exports = weave;
